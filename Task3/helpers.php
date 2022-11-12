6668e27c44b5033faccbc216c71d1d030663227c
Marek Zofota <mzofota@gmail.com>
2022-11-12 19:24:13 +0100

    Task 3 - Created two different function for check number is even or odd in helper.php

---
+++ Task3/helpers.php
@@ -0,0 +1,44 @@
+<?php
+
+// Here paste code for helper functions
+
+if (! function_exists('is_number_odd')) {
+    /**
+     * Check if is number odd
+     * @param int $number
+     * @return bool
+     */
+    function is_number_odd(int $number): bool
+    {
+//        if ( $number % 2 === 0) {
+//            return false;
+//        }
+//
+//        return true;
+
+        // Shorter notation
+        return !($number % 2 === 0);
+    }
+}
+
+if (! function_exists('what_number')) {
+    /**
+     * Check what is number
+     * @param mixed $number
+     * @return string
+     */
+    function what_number(mixed $number): string
+    {
+        if ( (int) $number % 2 == 0) {
+            return 'even';
+        }
+
+        return 'odd';
+    }
+}
+
+var_dump(is_number_odd(3));
+var_dump(is_number_odd(4));
+
+var_dump(what_number('3'));
+var_dump(what_number(4));
